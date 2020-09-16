c885ef46dc92534bb995e93a3e4c81905091fdc3
sergeykhargelia <sergey.khargelia@gmail.com>
2020-09-16 17:25:00 +0300

    added function prime in Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
+fun prime(n: Int): Boolean {
+    var currentDivisor = 2
+    while (currentDivisor * currentDivisor < n) {
+        if (n % currentDivisor == 0) {
+            return false
+        }
+        currentDivisor++
+    }
+    return true
+}
+
 fun main() {
 
 }
