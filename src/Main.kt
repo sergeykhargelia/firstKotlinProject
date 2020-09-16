f26e990adea36a05eccbf9ba97d5c7d888010483
sergeykhargelia <sergey.khargelia@gmail.com>
2020-09-16 17:27:06 +0300

    debugged function prime in Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun prime(n: Int): Boolean {
     var currentDivisor = 2
-    while (currentDivisor * currentDivisor < n) {
+    while (currentDivisor * currentDivisor <= n) {
         if (n % currentDivisor == 0) {
             return false
         }
